8d5bf88cffaed131fa7e0844d1929e314dc04a6b
dbriksza <dbriksza@gmail.com>
2019-09-10 13:33:44 -0400

    can't get timer working

--- src/App.js
+++ src/App.js
@@ -5,57 +5,68 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          {Team(Button("Home"))}
+          <div className="home">
+            <h2 className="home__name">Lions</h2>
+
+            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+
+            <div className="home__score">{homeScore}</div>
+          </div>
           <div className="timer">00:03</div>
-          {Team(Button("Away"))}
+          <div className="away">
+            <h2 className="away__name">Tigers</h2>
+            <div className="away__score">{awayScore}</div>
+          </div>
         </div>
         <BottomRow />
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          {Button("Home", "Touch Down")}
-          {Button("Home", "Field Goal")}
+          <button
+            onClick={() => setHomeScore(homeScore + 7)}
+            className="homeButtons__touchdown"
+          >
+            Home Touchdown
+          </button>
+          <button
+            onClick={() => setHomeScore(homeScore + 3)}
+            className="homeButtons__fieldGoal"
+          >
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          {Button("Away", "Touch Down")}
-          {Button("Away", "Field Goal")}
+          <button
+            onClick={() => setAwayScore(awayScore + 7)}
+            className="awayButtons__touchdown"
+          >
+            Away Touchdown
+          </button>
+          <button
+            onClick={() => setAwayScore(awayScore + 3)}
+            className="awayButtons__fieldGoal"
+          >
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
   );
 }
 
-function Button(team, scoreType) {
-  const [score, setScore] = useState(0);
-  return (
-    <button
-      onClick={() => {
-        if ((scoreType = "touchdown")) {
-          setScore(score + 7);
-        } else if ((scoreType = "fieldGoal")) {
-          setScore(score + 3);
-        }
-      }}
-      className="homeButtons__touchdown"
-    >
-      {team} {scoreType}
-    </button>
-  );
-}
+// function tick(secondsRemaining) {
+//   this.setState({ secondsRemaining: this.state.secondsRemaining - 1 });
+//   this.interval = setInterval(this.tick, 1000);
+//   return <div>Seconds Remaining: {this.state.secondsRemaining}</div>;
+// }
 
-function Team(cb) {
-  return (
-    <div className="away">
-      <h2 className="away__name">{cb.team}</h2>
-      <div className="away__score">{cb.score}</div>
-    </div>
-  );
-}
+// tick(30);
 
 export default App;
